700554ec0c3b6e1e856d03d8f429a3cd24f1f920
douglasato <102331650+douglasato@users.noreply.github.com>
2024-09-03 21:32:23 -0300

    Update TelaLogin.java

--- TelaLogin.java
+++ TelaLogin.java
@@ -33,6 +33,35 @@ public class TelaLogin extends JFrame{
         }
     );
 
+    JButtonLogin.addActionListener(
+        new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                try {
+                    if (txtLogin.getText().trim().length() == 0) {
+                            lblNotificacoes.setText(setHtmlFormat("É necessário digitar um login para acessar! Por favor, verifique e tente novamente."));
+                            txtLogin.requestFocus();
+                    } else if (String.valueOf(txtSenha.getPassword()).trim().length() == 0) {
+                            lblNotificacoes.setText(setHtmlFormat("É necessário digitar uma senha para acessar! Por favor, verifique e tente novamente."));
+                            txtSenha.requestFocus();
+                    } else {
+                            Connection conexao = MySQLConnector.conectar();
+                            String strSqlLogin = "select * from `" + dbPadrao + "`.`" + tblPadrao + "`" + " where `email` = '" + txtLogin.getText() + "' and `senha` = '" + String.valueOf(txtSenha.getPassword()) + "';";
+                            Statement stmSqlLogin = conexao.createStatement();
+                            ResultSet rstSqlLogin = stmSqlLogin.executeQuery(strSqlLogin);
+                    if (rstSqlLogin.next()) {
+                                MenuFrame appMenuFrame = new MenuFrame();
+                                appMenuFrame.setDefaultCloseOperation(ABORT);
+                    } else {
+                                lblNotificacoes.setText(setHtmlFormat("Não foi encontrado o login e/ou senha digitados! Por favor, verifique e tente novamente."));
+                     }
+                    }
+                } catch (Exception e) {
+                        lblNotificacoes.setText(setHtmlFormat("Ops! Deu ruim no banco de dados, veja o erro: " + e));
+                  }
+                }
+            }
+    );
+
     add(login);
     add(notificacao); 
     add(new JLabel());
