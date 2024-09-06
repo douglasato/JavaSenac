c164fab2218491ebf32d8a8be2e8fa0ec310a523
douglasato <102331650+douglasato@users.noreply.github.com>
2024-09-06 16:31:25 -0300

    Add files via upload

--- TerminalCalculator/src/Usuario.java
+++ TerminalCalculator/src/Usuario.java
@@ -13,7 +13,7 @@ public class Usuario {
             System.out.println("Digite a opção que deseja escolher: ");
             System.out.println("1 - Pratos executivos\n2 - Bebidas\n3 - Sobremesas ");
             opcao = scan.nextInt();
-            
+
             switch (opcao) {
                 case 1:
                     System.out.println("Opções:\n1: Hambúrguer com fritas, arroz, feijão e salada\n2: Filé mignon com fritas, arroz, feijão e salada mista\n3: Bife à parmegiana com fritas, arroz, feijão e salada mista");
@@ -32,36 +32,49 @@ public class Usuario {
             }
             
             if(opcao == 1){
-                if(opcoes == 1){
-                    System.out.println("Você escolheu a opção "+opcao+"-Pratos executivos e solicitou o prato executivo nº "+opcoes+": Hambúrguer com fritas, arroz, feijão e salada");
-                }
-                if(opcoes == 2){
-                    System.out.println("Você escolheu a opção "+opcao+"-Pratos executivos e solicitou o prato executivo nº "+opcoes+": Filé mignon com fritas, arroz, feijão e salada mista");
-                }
-                if(opcoes == 3){
-                    System.out.println("Você escolheu a opção "+opcao+"-Pratos executivos e solicitou o prato executivo nº "+opcoes+": Bife à parmegiana com fritas, arroz, feijão e salada mista");
+                switch (opcoes) {
+                    case 1:
+                        System.out.println("Você escolheu a opção:\n"+opcao+"-Pratos executivos e solicitou o prato executivo nº "+opcoes+": Hambúrguer com fritas, arroz, feijão e salada");
+                        break;
+                    case 2:
+                        System.out.println("Você escolheu a opção:\n"+opcao+"-Pratos executivos e solicitou o prato executivo nº "+opcoes+": Filé mignon com fritas, arroz, feijão e salada mista");
+                        break;
+                    case 3:
+                        System.out.println("Você escolheu a opção:\n"+opcao+"-Pratos executivos e solicitou o prato executivo nº "+opcoes+": Bife à parmegiana com fritas, arroz, feijão e salada mista");
+                        break;
+                    default:
+                        break;
                 }
             }
             if(opcao == 2){
-                if(opcoes == 1){
-                    System.out.println("Você escolheu a opção "+opcao+"-Bebidas e solicitou a opção nº "+opcoes+": Sucos naturais");
-                }
-                if(opcoes == 2){
-                    System.out.println("Você escolheu a opção "+opcao+"-Bebidas e solicitou a opção nº "+opcoes+": Refrigerantes");
-                }
-                if(opcoes == 3){
-                    System.out.println("Você escolheu a opção "+opcao+"-Bebidas e solicitou a opção nº "+opcoes+": Chopp");
+                switch (opcoes) {
+                    case 1:
+                        System.out.println("Você escolheu a opção:\n"+opcao+"-Bebidas e solicitou a opção nº "+opcoes+": Sucos naturais");
+                        break;
+                    case 2:
+                        System.out.println("Você escolheu a opção:\n"+opcao+"-Bebidas e solicitou a opção nº "+opcoes+": Refrigerantes");
+                        break;
+                    case 3:
+                        System.out.println("Você escolheu a opção:\n"+opcao+"-Bebidas e solicitou a opção nº "+opcoes+": Chopp");
+                        break;
+                    default:
+                    break;
+
                 }
             }
             if(opcao == 3){
-                if(opcoes == 1){
-                    System.out.println("Você escolheu a opção "+opcao+"-Sobremesas e solicitou a opção nº "+opcoes+": Sorvete");
-                }
-                if(opcoes == 2){
-                    System.out.println("Você escolheu a opção "+opcao+"-Sobremesas e solicitou a opção nº "+opcoes+": Torta holandesa");
-                }
-                if(opcoes == 3){
-                    System.out.println("Você escolheu a opção "+opcao+"-Sobremesas e solicitou a opção nº "+opcoes+": Bolo");
+                switch (opcoes) {
+                    case 1:
+                        System.out.println("Você escolheu a opção:\n"+opcao+"-Sobremesas e solicitou a opção nº "+opcoes+": Sorvete");
+                        break;
+                    case 2:
+                        System.out.println("Você escolheu a opção:\n"+opcao+"-Sobremesas e solicitou a opção nº "+opcoes+": Torta holandesa");
+                        break;
+                    case 3:
+                        System.out.println("Você escolheu a opção:\n"+opcao+"-Sobremesas e solicitou a opção nº "+opcoes+": Bolo");
+                        break;
+                    default:
+                        break;
                 }
             }
         System.out.println("Deseja acrescentar mais alguma coisa no seu pedido?\nDigite 1 para incrementar seu pedido.");
