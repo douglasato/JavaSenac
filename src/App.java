3e907288581934fa255794b48c2c1210b526593e
douglasato <102331650+douglasato@users.noreply.github.com>
2024-10-08 16:47:01 -0300

    Delete src directory

--- src/App.java
+++
@@ -1,8 +0,0 @@
-import java.sql.Connection;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Connection conexao = MySQLConnector.conectar();
-    }
-}
